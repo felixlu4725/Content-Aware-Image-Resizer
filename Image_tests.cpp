edc9678a648430bf2d1f63d2a46e2bf3503d4136
qec777 <123790633+qec777@users.noreply.github.com>
2023-01-29 16:04:40 -0500

    Created basic tests for all functions
    Will add more tests

--- Image_tests.cpp
+++ Image_tests.cpp
@@ -10,6 +10,10 @@
 
 using namespace std;
 
+const Pixel red = {255, 0, 0};
+const Pixel green = {0, 255, 0};
+const Pixel blue = {0, 0, 255};
+const Pixel white = {255, 255, 255};
 
 // Here's a free test for you! Model yours after this one.
 // Test functions have no interface and thus no RMEs, but
@@ -19,11 +23,6 @@ using namespace std;
 // that Image_print produces the correct output.
 TEST(test_print_basic) {
   Image *img = new Image; // create an Image in dynamic memory
-
-  const Pixel red = {255, 0, 0};
-  const Pixel green = {0, 255, 0};
-  const Pixel blue = {0, 0, 255};
-  const Pixel white = {255, 255, 255};
 
   Image_init(img, 2, 2);
   Image_set_pixel(img, 0, 0, red);
@@ -47,6 +46,94 @@ TEST(test_print_basic) {
 
 // IMPLEMENT YOUR TEST FUNCTIONS HERE
 // You are encouraged to use any functions from Image_test_helpers.h as needed.
+
+TEST(test_image_print) {
+  Image *img = new Image;
+  Image_init(img, 1, 1);
+
+  Image_set_pixel(img, 0, 0, white);
+  ostringstream expected;
+  expected << "P3\n"
+           << "1 1\n"
+           << "255\n"
+           << "255 255 255 \n";
+  ostringstream actual;
+  Image_print(img, actual);
+
+  ASSERT_EQUAL(expected.str(), actual.str());
+
+  delete img;
+}
+
+TEST(test_image_width) {
+  Image *img = new Image;
+  Image_init(img, 1, 2);
+
+  ASSERT_EQUAL(Image_width(img), 1);
+
+  delete img;
+}
+
+TEST(test_image_height) {
+  Image *img = new Image;
+  Image_init(img, 1, 2);
+
+  ASSERT_EQUAL(Image_height(img), 2);
+
+  delete img;
+}
+
+TEST(test_image_get_pixel) {
+  Image *img = new Image;
+
+  Image_init(img, 2, 2);
+  Image_set_pixel(img, 0, 0, red);
+  Image_set_pixel(img, 0, 1, green);
+  Image_set_pixel(img, 1, 0, blue);
+  Image_set_pixel(img, 1, 1, white);
+
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).r, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).g, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).b, 255);
+
+  delete img;
+}
+
+TEST(test_image_set_pixel) {
+  Image *img = new Image;
+
+  Image_init(img, 2, 2);
+  Image_set_pixel(img, 0, 0, red);
+  Image_set_pixel(img, 0, 1, green);
+  Image_set_pixel(img, 1, 0, blue);
+  Image_set_pixel(img, 1, 1, white);
+
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).r, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).g, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).b, 255);
+
+  delete img;
+}
+
+TEST(test_image_fill) {
+  Image *img = new Image;
+
+  Image_init(img, 2, 2);
+  Image_fill(img, white);
+
+  ostringstream expected;
+  expected << "P3\n"
+           << "2 2\n"
+           << "255\n"
+           << "255 255 255 255 255 255 \n"
+           << "255 255 255 255 255 255 \n";
+  ostringstream actual;
+  Image_print(img, actual);
+
+  ASSERT_EQUAL(expected.str(), actual.str());
+
+  delete img;
+}
 
 // NOTE: The unit test framework tutorial in Lab 2 originally
 // had a semicolon after TEST_MAIN(). Although including and
