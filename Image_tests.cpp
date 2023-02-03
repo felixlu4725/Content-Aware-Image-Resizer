c5e0d22b08f5fbc602bb8a4055c65bddd5d3ed93
qec777 <qec@umich.edu>
2023-02-03 16:33:08 -0500

    added more Image_tests.cpp

--- Image_tests.cpp
+++ Image_tests.cpp
@@ -21,7 +21,7 @@ const Pixel white = {255, 255, 255};
 // -----
 // Sets various pixels in a 2x2 Image and checks
 // that Image_print produces the correct output.
-TEST(test_print_basic) {
+TEST(test_print_nxn) {
   Image *img = new Image; // create an Image in dynamic memory
 
   Image_init(img, 2, 2);
@@ -47,7 +47,7 @@ TEST(test_print_basic) {
 // IMPLEMENT YOUR TEST FUNCTIONS HERE
 // You are encouraged to use any functions from Image_test_helpers.h as needed.
 
-TEST(test_print_edge) {
+TEST(test_print_1x1) {
   Image *img = new Image;
   Image_init(img, 1, 1);
 
@@ -65,17 +65,54 @@ TEST(test_print_edge) {
   delete img;
 }
 
-TEST(test_image_init) {
+TEST(test_print_1xn) {
   Image *img = new Image;
-  Image_init(img, 1, 1);
 
-  ASSERT_EQUAL(Image_width(img), 1);
-  ASSERT_EQUAL(Image_height(img), 1);
+  Image_init(img, 1, 2);
+  Image_set_pixel(img, 0, 0, red);
+  Image_set_pixel(img, 1, 0, blue);
+
+  ostringstream s;
+  Image_print(img, s);
+
+  ostringstream correct;
+  correct << "P3\n1 2\n255\n";
+  correct << "255 0 0 \n";
+  correct << "0 0 255 \n";
+  ASSERT_EQUAL(s.str(), correct.str());
 
   delete img;
 }
 
-TEST(test_image_init_edge) {
+TEST(test_print_nx1) {
+  Image *img = new Image;
+
+  Image_init(img, 2, 1);
+  Image_set_pixel(img, 0, 0, red);
+  Image_set_pixel(img, 0, 1, green);
+
+  ostringstream s;
+  Image_print(img, s);
+
+  ostringstream correct;
+  correct << "P3\n2 1\n255\n";
+  correct << "255 0 0 0 255 0 \n";
+  ASSERT_EQUAL(s.str(), correct.str());
+
+  delete img;
+}
+
+TEST(test_image_init_nxn) {
+  Image *img = new Image;
+  Image_init(img, 2, 2);
+
+  ASSERT_EQUAL(Image_width(img), 2);
+  ASSERT_EQUAL(Image_height(img), 2);
+
+  delete img;
+}
+
+TEST(test_image_init_fromInput) {
   Image *img = new Image;
   string input = "P3 2 2\t255 5 0 1\n0\n255 \n0 \n0 0 90 \t255 \n255 \t45 \n";
   std::istringstream ss_input(input);
@@ -112,15 +149,12 @@ TEST(test_image_height) {
 TEST(test_image_get_pixel) {
   Image *img = new Image;
 
-  Image_init(img, 2, 2);
-  Image_set_pixel(img, 0, 0, red);
-  Image_set_pixel(img, 0, 1, green);
-  Image_set_pixel(img, 1, 0, blue);
-  Image_set_pixel(img, 1, 1, white);
+  Image_init(img, 1, 1);
+  Image_set_pixel(img, 0, 0, white);
 
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).r, 255);
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).g, 255);
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).b, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).r, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).g, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).b, 255);
 
   delete img;
 }
@@ -128,20 +162,17 @@ TEST(test_image_get_pixel) {
 TEST(test_image_set_pixel) {
   Image *img = new Image;
 
-  Image_init(img, 2, 2);
-  Image_set_pixel(img, 0, 0, red);
-  Image_set_pixel(img, 0, 1, green);
-  Image_set_pixel(img, 1, 0, blue);
-  Image_set_pixel(img, 1, 1, white);
+  Image_init(img, 1, 1);
+  Image_set_pixel(img, 0, 0, white);
 
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).r, 255);
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).g, 255);
-  ASSERT_EQUAL(Image_get_pixel(img, 1, 1).b, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).r, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).g, 255);
+  ASSERT_EQUAL(Image_get_pixel(img, 0, 0).b, 255);
 
   delete img;
 }
 
-TEST(test_image_fill) {
+TEST(test_image_fill_nxn) {
   Image *img = new Image;
 
   Image_init(img, 2, 2);
@@ -153,6 +184,64 @@ TEST(test_image_fill) {
            << "255\n"
            << "255 255 255 255 255 255 \n"
            << "255 255 255 255 255 255 \n";
+  ostringstream actual;
+  Image_print(img, actual);
+
+  ASSERT_EQUAL(expected.str(), actual.str());
+
+  delete img;
+}
+
+TEST(test_image_fill_1xn) {
+  Image *img = new Image;
+
+  Image_init(img, 1, 2);
+  Image_fill(img, white);
+
+  ostringstream expected;
+  expected << "P3\n"
+           << "1 2\n"
+           << "255\n"
+           << "255 255 255 \n"
+           << "255 255 255 \n";
+  ostringstream actual;
+  Image_print(img, actual);
+
+  ASSERT_EQUAL(expected.str(), actual.str());
+
+  delete img;
+}
+
+TEST(test_image_fill_nx1) {
+  Image *img = new Image;
+
+  Image_init(img, 2, 1);
+  Image_fill(img, white);
+
+  ostringstream expected;
+  expected << "P3\n"
+           << "2 1\n"
+           << "255\n"
+           << "255 255 255 255 255 255 \n";
+  ostringstream actual;
+  Image_print(img, actual);
+
+  ASSERT_EQUAL(expected.str(), actual.str());
+
+  delete img;
+}
+
+TEST(test_image_fill_1x1) {
+  Image *img = new Image;
+
+  Image_init(img, 1, 1);
+  Image_fill(img, white);
+
+  ostringstream expected;
+  expected << "P3\n"
+           << "1 1\n"
+           << "255\n"
+           << "255 255 255 \n";
   ostringstream actual;
   Image_print(img, actual);
 
