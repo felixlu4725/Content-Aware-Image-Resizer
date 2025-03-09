54d9eb38fbcff1aa9013ddc897fcb19777d0614d
Felix Lu <112906023+felixlu4725@users.noreply.github.com>
2025-03-09 11:32:07 -0400

    Update Matrix_tests.cpp

--- Matrix_tests.cpp
+++ Matrix_tests.cpp
@@ -1,4 +1,3 @@
-// Project UID af1f95f547e44c8ea88730dfb185559d
 
 #include "Matrix.h"
 #include "Matrix_test_helpers.h"
@@ -6,13 +5,6 @@
 
 using namespace std;
 
-
-// Here's a free test for you! Model yours after this one.
-// Test functions have no interface and thus no RMEs, but
-// add a comment like the one here to say what it is testing.
-// -----
-// Fills a 3x5 Matrix with a value and checks
-// that Matrix_at returns that value for each element.
 TEST(test_fill_basic) {
   Matrix *mat = new Matrix; // create a Matrix in dynamic memory
 
@@ -396,10 +388,4 @@ TEST(test_matrix_at_const) {
     delete mat;
 }
 
-// NOTE: The unit test framework tutorial in Lab 2 originally
-// had a semicolon after TEST_MAIN(). Although including and
-// excluding the semicolon are both correct according to the c++
-// standard, the version of g++ we are using reports a warning
-// when there is a semicolon. Therefore do NOT add a semicolon
-// after TEST_MAIN()
 TEST_MAIN() // Do NOT put a semicolon here
