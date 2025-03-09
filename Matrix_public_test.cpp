6cb531db60346c86bfeebccd758c4829df7f007c
Felix Lu <112906023+felixlu4725@users.noreply.github.com>
2025-03-09 11:31:04 -0400

    Update Matrix_public_test.cpp

--- Matrix_public_test.cpp
+++ Matrix_public_test.cpp
@@ -1,4 +1,3 @@
-// Project UID af1f95f547e44c8ea88730dfb185559d
 
 #include "Matrix.h"
 #include "Matrix_test_helpers.h"
@@ -6,14 +5,6 @@
 #include <sstream>
 
 using std::ostringstream;
-
-
-// This is the public Matrix test for which the autograder gives feedback.
-// It only tests VERY FEW of the expected behaviors of the Matrix module.
-// It will only really tell you if your code compiles and you remembered to
-// write the functions. It is not to be trusted. It tells the truth, but not
-// the whole truth. It might put you in a blender. You get the point.
-// You must write your own comprehensive unit tests in Matrix_tests.cpp!
 
 
 TEST(test_matrix_basic) {
